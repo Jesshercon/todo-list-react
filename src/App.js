bf4303c23a9e21cc418f78b9d424a779d28d11f4
jessica <jessicahercon@gmail.com>
2023-01-16 18:40:37 -0600

    input can identify the target of input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import TaskList from './components/TaskList';
+import { useState } from 'react';
 
 function App() {
+const [inputString, setInputString]=useState("")
+
+const getInputText=(event)=>{
+  console.log(event.target.value)
+}
+
   return (
     <div className="App">
    
@@ -11,7 +19,9 @@ function App() {
   <label className='text-xl font-semibold'> Insert your task here: </label>
 
   <div className=''>
-  <input className='input-task border border-gray-500 rounded-sm px-2 shadow-sm' type="text" placeholder='Type your task' />
+  <input className='input-task border border-gray-500 rounded-sm px-2 shadow-sm' 
+  type="text" placeholder='Type your task' value={inputString}
+  onChange={getInputText} />
   <button className='mx-4 px-2 py-1 text-white bg-blue-400 rounded'>Add task</button>
   </div>
 
---
+++ src/components/TaskList.js
@@ -0,0 +1,17 @@
+const TaskList= ()=>{
+
+
+
+    return(
+        <div>
+            <li>
+
+            </li>
+            <button>Edit</button>
+            <button>Delete</button>
+        </div>
+    )
+}
+
+export default TaskList;
+
