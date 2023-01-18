e35dbd607132d9b92a1593f63f78ce980a7e2fa7
jessica <jessicahercon@gmail.com>
2023-01-17 18:19:45 -0600

    changes added on app.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
 const [inputString, setInputString]=useState("")
 
 const getInputText=(event)=>{
-  console.log(event.target.value)
+  setInputString(event.target.value)  
 }
+
 
   return (
     <div className="App">
@@ -18,11 +19,12 @@ const getInputText=(event)=>{
 <form className='my-10'>
   <label className='text-xl font-semibold'> Insert your task here: </label>
 
-  <div className=''>
+  <div className='mt-6'>
   <input className='input-task border border-gray-500 rounded-sm px-2 shadow-sm' 
   type="text" placeholder='Type your task' value={inputString}
   onChange={getInputText} />
-  <button className='mx-4 px-2 py-1 text-white bg-blue-400 rounded'>Add task</button>
+  <button className='mx-4 px-2 py-1 text-white bg-blue-400 rounded' type='submit'>Add task</button>
+  <p className='mt-6'>You added the following tasks: {inputString}</p>
   </div>
 
 </form>
